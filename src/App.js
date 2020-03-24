e0e85604e74f11e9ee1468b37569532ff7225068
Yacob Madiana <yacob.madiana@gmail.com>
2020-03-24 13:17:24 +0700

    Add GraphQL mutation create

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import { createNote } from './graphql/mutations';
 
-function App() {
-  return <div>App</div>;
+class App extends React.Component {
+  state = {
+    note: '',
+    notes: []
+  };
+
+  handleChangeNote = event => {
+    this.setState({ note: event.target.value });
+  };
+  handleAddNote = event => {
+    const { note } = this.state;
+    event.preventDefault();
+    const input = {
+      note: note
+    };
+    API.graphql(graphqlOperation(createNote, { input: input }));
+  };
+  render() {
+    const { notes } = this.state;
+
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-1">Amplify Notetaker</h1>
+        {/* Note Form */}
+        <form className="mb3" onSubmit={this.handleAddNote}>
+          <input
+            type="text"
+            className="pa2 f4"
+            placeholder="Write your note"
+            onChange={this.handleChangeNote}
+          />
+          <button className="pa2 f4" type="submit">
+            Add Note
+          </button>
+        </form>
+
+        {/* Note Lists */}
+        <div>
+          {notes.map(item => (
+            <div key={item.id} className="flex items-center">
+              <li className="list pa1 f3">{item.note}</li>
+              <button className="bg-transparent bn f4">
+                <span>&times;</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
